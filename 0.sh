5dfcf93b7bfd532063ebccab6759663db9efb9c4
lmarinve <lmarinve@gmail.com>
2022-08-01 10:45:08 -0400

    kytos setup

--- 0.sh
+++ 0.sh
@@ -1,42 +1,18 @@
 #!/bin/sh
-multipass exec sdx -- bash -c "sudo apt-get install --assume-yes --no-install-recommends \
-                build-essential \
-		ca-certificates \
-		dirmngr \
-		dpkg-dev \
-		gcc \
-                git \
-		gnupg \
-                gunicorn \
-                iputils-ping \
-		libbz2-dev \
-		libc6-dev \
-		libexpat1-dev \
-		libffi-dev \
-		liblzma-dev \
-                libncurses5-dev \
-                libgdbm-dev \
-                libnss3-dev \
-                libreadline-dev \
-		libsqlite3-dev \
-		libssl-dev \
-                lsof \
-		make \
-                mininet \
-		netbase \
-                netcat \
-                software-properties-common \
-		uuid-dev \
-		wget \
-		xz-utils \
-		zlib1g-dev"
-multipass exec sdx -- bash -c "sudo apt install python3-pip --assume-yes"
-multipass exec sdx -- bash -c "sudo apt-get purge --assume-yes --auto-remove -o APT::AutoRemove::RecommendsImportant=false && \
-sudo rm -rf /var/lib/apt/lists/*"
-multipass exec sdx -- bash -c "pip3 install --upgrade pip"
-# multipass exec sdx -- bash -c "pip3 install six==1.16.0"
-multipass exec sdx -- bash -c "pip3 install wheel==0.37.0"
-multipass exec sdx -- bash -c "pip3 install grpcio==1.41.0"
-multipass exec sdx -- bash -c "pip3 install networkx==2.5.1"
-multipass exec sdx -- bash -c "pip3 install eventlet==0.33.0"
-multipass exec sdx -- bash -c "pip3 install black==20.8b0"
+echo "### set work directory ###"
+multipass exec sdx -- bash -c "sudo mkdir /var/lib/kytos"
+multipass exec sdx -- bash -c "sudo mkdir /var/run/kytos"
+multipass exec sdx -- bash -c "sudo mkdir /var/tmp/kytos"
+multipass exec sdx -- bash -c "sudo mkdir /etc/kytos"
+multipass exec sdx -- bash -c "sudo mkdir -p /usr/src/kytos"
+multipass exec sdx -- bash -c "sudo chown ubuntu:ubuntu /usr/src/kytos"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/python-openflow"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/kytos-utils"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/kytos"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/flow_manager"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/mef_eline"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/of_core"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/of_lldp"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/pathfinder"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://github.com/kytos-ng/topology"
+multipass exec sdx -- bash -c "cd /usr/src/kytos && git clone https://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@github.com/atlanticwave-sdx/kytos-sdx-topology sdx_topology"
